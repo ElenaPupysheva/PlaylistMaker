65a7ee234b2d2c67320d2136c23a65541c226221
Alonso <nyankem@gmail.com>
2024-09-28 21:27:02 +0300

    Выполнены изменения по исправлению ошибки и рекомендациям

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -2,10 +2,11 @@ package com.practicum.playlistmaker
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,12 +16,15 @@ class SettingsActivity : AppCompatActivity() {
         //    v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
         //    insets
         //}
-        val backButton = findViewById<Button>(R.id.back_button)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
-        backButton.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
+        // Enable the navigation icon (back button)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toolbar.setNavigationOnClickListener {
+             val intent = Intent(this, MainActivity::class.java)
+             startActivity(intent)
+             finish()
         }
     }
 }
