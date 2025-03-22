5a933ff0413ac59d66dc446e553ecdb773ff8c45
ElenaPupysheva <nyankem@gmail.com>
2025-03-22 20:40:15 +0300

    Реализация SettingsFragment

---
+++ app/src/main/java/com/practicum/playlistmaker/settings/ui/SettingsFragment.kt
@@ -0,0 +1,81 @@
+package com.practicum.playlistmaker.settings.ui
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.practicum.playlistmaker.App
+import com.practicum.playlistmaker.R
+import com.practicum.playlistmaker.databinding.FragmentSettingsBinding
+import com.practicum.playlistmaker.settings.presentation.SettingsViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class SettingsFragment : Fragment() {
+    private var _binding: FragmentSettingsBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: SettingsViewModel by viewModel()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.darkThemeEnabled.observe(viewLifecycleOwner) { isEnabled ->
+            val app = requireActivity().application as App
+            if (app.darkTheme != isEnabled) {
+                app.switchTheme(isEnabled)
+            }
+            binding.themeSwitcher.isChecked = isEnabled
+        }
+
+        binding.themeSwitcher.setOnCheckedChangeListener { _, isChecked ->
+            viewModel.onThemeToggled(isChecked)
+        }
+
+        binding.appShare.setOnClickListener {
+            val shareMessage = getString(R.string.link_course)
+            val shareIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, shareMessage)
+                type = "text/plain"
+            }
+            val chooser = Intent.createChooser(shareIntent, getString(R.string.share_text))
+            startActivity(chooser)
+        }
+
+        binding.mailSupport.setOnClickListener {
+            val message = getString(R.string.message_text)
+            val theme = getString(R.string.theme_text)
+            val shareIntent = Intent(Intent.ACTION_SENDTO)
+            shareIntent.data = Uri.parse("mailto:")
+            shareIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.link_mail)))
+            shareIntent.putExtra(Intent.EXTRA_TEXT, message)
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, theme)
+            startActivity(shareIntent)
+        }
+
+        binding.userAgreement.setOnClickListener {
+            val browserIntent =
+                Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.link_agreement)))
+            startActivity(browserIntent)
+        }
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
+
