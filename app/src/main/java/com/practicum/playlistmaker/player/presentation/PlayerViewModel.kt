8f021bca064024d9c42c86063ec81f6ff8033441
ElenaPupysheva <nyankem@gmail.com>
2025-04-13 18:09:27 +0300

    Спринт 20

--- app/src/main/java/com/practicum/playlistmaker/player/presentation/PlayerViewModel.kt
+++ app/src/main/java/com/practicum/playlistmaker/player/presentation/PlayerViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.practicum.playlistmaker.player.data.dto.PlayerStates
 import com.practicum.playlistmaker.player.domain.api.PlayerInteractor
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
@@ -21,18 +22,22 @@ class PlayerViewModel(private val playerInteractor: PlayerInteractor) : ViewMode
     )
     val uiState: LiveData<PlayerUiState> = _uiState
 
-    private val updateInterval = 1000L
+    private val updateInterval = 300L
+    private var updateJob: Job? = null
 
     fun preparePlayer(url: String) {
         playerInteractor.preparePlayer(
             url = url,
             onPrepared = {
-                _uiState.value = _uiState.value?.copy(
-                    playerState = PlayerStates.PREPARED,
-                    currentTime = "0:00"
+                _uiState.postValue(
+                    PlayerUiState(
+                        playerState = PlayerStates.PREPARED,
+                        currentTime = "0:00"
+                    )
                 )
             },
             onCompletion = {
+                stopUpdatingProgress()
                 _uiState.postValue(
                     PlayerUiState(
                         playerState = PlayerStates.PREPARED,
@@ -51,7 +56,8 @@ class PlayerViewModel(private val playerInteractor: PlayerInteractor) : ViewMode
 
     fun pausePlayer() {
         playerInteractor.pausePlayer()
-        _uiState.value = _uiState.value?.copy(playerState = PlayerStates.PAUSED)
+        _uiState.postValue(_uiState.value?.copy(playerState = PlayerStates.PAUSED))
+        stopUpdatingProgress()
     }
 
     fun playbackControl() {
@@ -62,9 +68,16 @@ class PlayerViewModel(private val playerInteractor: PlayerInteractor) : ViewMode
         }
     }
 
+    private fun stopUpdatingProgress() {
+        updateJob?.cancel()
+        updateJob = null
+    }
+
     private fun startUpdatingProgress() {
-        viewModelScope.launch {
-            while (_uiState.value?.playerState == PlayerStates.PLAYING) {
+        stopUpdatingProgress()
+
+        updateJob = viewModelScope.launch {
+            while (playerInteractor.isPlaying()) {
                 val newTime = SimpleDateFormat("mm:ss", Locale.getDefault())
                     .format(playerInteractor.getCurrentPositionMs())
 
@@ -76,6 +89,7 @@ class PlayerViewModel(private val playerInteractor: PlayerInteractor) : ViewMode
 
     override fun onCleared() {
         super.onCleared()
+        stopUpdatingProgress()
         playerInteractor.releasePlayer()
     }
 }
