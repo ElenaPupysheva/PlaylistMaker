0226972e47180c94d0ccea6612465bf24733b86f
ElenaPupysheva <nyankem@gmail.com>
2025-06-14 00:26:28 +0300

    Использование bitmap и смена метода onClick

--- app/src/main/java/com/practicum/playlistmaker/player/ui/custom/PlaybackButtonView.kt
+++ app/src/main/java/com/practicum/playlistmaker/player/ui/custom/PlaybackButtonView.kt
@@ -1,13 +1,78 @@
 package com.practicum.playlistmaker.player.ui.custom
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.RectF
 import android.util.AttributeSet
-import androidx.appcompat.widget.AppCompatImageView
+import android.view.MotionEvent
+import android.view.View
+import androidx.annotation.AttrRes
+import androidx.annotation.StyleRes
+import androidx.core.graphics.drawable.toBitmap
+import com.practicum.playlistmaker.R
 
 class PlaybackButtonView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
-    defStyleAttr: Int = 0
-) : AppCompatImageView(context, attrs, defStyleAttr) {
-    //TODO
+    @AttrRes defStyleAttr: Int = 0,
+    @StyleRes defStyleRes: Int = 0,
+) : View(context, attrs, defStyleAttr, defStyleRes) {
+
+    private var playIcon: Bitmap? = null
+    private var pauseIcon: Bitmap? = null
+
+    var isPlaying: Boolean = false
+        set(value) {
+            field = value
+            invalidate()
+        }
+
+    private var clickListener: (() -> Unit)? = null
+
+    init {
+        context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.PlaybackButtonView,
+            defStyleAttr,
+            defStyleRes
+        ).apply {
+            try {
+                playIcon = getDrawable(R.styleable.PlaybackButtonView_playIcon)?.toBitmap()
+                pauseIcon = getDrawable(R.styleable.PlaybackButtonView_pauseIcon)?.toBitmap()
+            } finally {
+                recycle()
+            }
+        }
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> return true
+            MotionEvent.ACTION_UP -> {
+                clickListener?.invoke()
+                performClick()
+                return true
+            }
+        }
+        return super.onTouchEvent(event)
+    }
+
+    override fun performClick(): Boolean {
+        super.performClick()
+        return true
+    }
+
+    fun setOnPlaybackClickListener(listener: () -> Unit) {
+        this.clickListener = listener
+    }
+
+    override fun onDraw(canvas: Canvas) {
+
+        val iconToDraw = if (isPlaying) pauseIcon else playIcon
+        iconToDraw?.let {
+            val rect = RectF(0f, 0f, width.toFloat(), height.toFloat())
+            canvas.drawBitmap(it, null, rect, null)
+        }
+    }
 }
