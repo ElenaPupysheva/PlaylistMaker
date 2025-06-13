7f9e59bc90172e648698faa7d9360ad853761e55
ElenaPupysheva <nyankem@gmail.com>
2025-06-13 22:48:35 +0300

    Создание класса PlaybackButtonView

---
+++ app/src/main/java/com/practicum/playlistmaker/player/ui/custom/PlaybackButtonView.kt
@@ -0,0 +1,13 @@
+package com.practicum.playlistmaker.player.ui.custom
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatImageView
+
+class PlaybackButtonView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : AppCompatImageView(context, attrs, defStyleAttr) {
+    //TODO
+}
